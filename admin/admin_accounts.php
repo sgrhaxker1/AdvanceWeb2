157726e386ec796436757e93cb4ef3548ce2f455
Chhiring03 <984070@win.edu.au>
2025-10-06 16:51:09 +1100

    Update admin_accounts.php fixed

--- admin/admin_accounts.php
+++ admin/admin_accounts.php
@@ -1,4 +1,21 @@
+<?php
 
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_admins = $conn->prepare("DELETE FROM `admins` WHERE id = ?");
+   $delete_admins->execute([$delete_id]);
+   header('location:admin_accounts.php');
+}
 
 ?>
 
@@ -74,3 +91,4 @@
    
 </body>
 </html>
+
