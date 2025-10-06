67b88c819c44f2bf1259f99b63a31910cdbaeb5e
Sagar Dhakal <100487057+sgrhaxker1@users.noreply.github.com>
2025-10-06 05:30:28 +0000

    updated all files

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:6100000000"><i class="fas fa-phone"></i> +61 5 1234 5678</a>
+         <a href="tel:6100000000"><i class="fas fa-phone"></i> +61 6 3456 7890</a>
+         <a href="support@aussiecart.com.au"><i class="fas fa-envelope"></i> support@shopmate.com.au</a>
+         <a href="https://www.google.com/maps?sca_esv=6449538529ffda2f&output=search&q=sydney+australia&source=lnms&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J3ppPdoHI1O-XvbXbpNjYYwWUVH6qTfR1Lpek5F-7GS5kHUv-XPg6sWhVG4k1EjbnJtLhBeL57sTXxXmiHxC27t3XXUxCvX_qlf7Bkns-G8lz6MIVRlNzq1Cqfjzvt-wjRfYPf8&entry=mc&ved=1t:200715&ictx=111"><i class="fas fa-map-marker-alt"></i> Sydney, Australia </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>ShopMate</span> | all rights reserved!</div>
+
+</footer>
